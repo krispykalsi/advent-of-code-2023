75cf22d061a8652f66c3210a82c8215a0c41f68b
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2023-12-05 19:25:38 +0530

    solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,85 @@
+class NumberTrie : Trie(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+) {
+    private val wordToIntMappings: Map<String, Int> = hashMapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+            "zero" to 0,
+    )
+
+    fun findNumber(string: String): Int? {
+        var node: Node? = root
+        var word = ""
+        for (ch in string) {
+            node = node?.getChild(ch)
+            if (node == null || node.value != ch) {
+                return null
+            }
+            word += node.value
+            if (node.isWordEnd()) {
+                return parseWordOrDigitToNumber(word)
+            }
+        }
+        return null
+    }
+
+    private fun parseWordOrDigitToNumber(wordOrDigit: String): Int {
+        return wordToIntMappings[wordOrDigit] ?: wordOrDigit.toInt()
+    }
+
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            var firstNum = ' '
+            var lastNum = ' '
+            for (ch in line) {
+                if (!ch.isDigit()) {
+                    continue
+                }
+                if (firstNum == ' ') {
+                    firstNum = ch
+                }
+                lastNum = ch
+            }
+            sum += "$firstNum$lastNum".toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numberTrie = NumberTrie()
+
+        var sum = 0
+        for (line in input) {
+            var firstNum = -1
+            var lastNum = -1
+            for (i in line.indices) {
+                val num = numberTrie.findNumber(line.substring(i)) ?: continue
+                if (firstNum == -1) {
+                    firstNum = num
+                }
+                lastNum = num
+            }
+
+            sum += "$firstNum$lastNum".toInt()
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -12,10 +12,63 @@ fun readInput(name: String) = Path("src/$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+        .toString(16)
+        .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+open class Trie(vararg words: String) {
+    class Node(val value: Char, private var isWord: Boolean = false) {
+        private val children = mutableMapOf<Char, Node>()
+
+        fun addChild(char: Char): Node {
+            if (!children.containsKey(char)) {
+                children[char] = Node(char)
+            }
+            return children[char]!!
+        }
+
+        fun hasChild(char: Char): Boolean {
+            return children.containsKey(char)
+        }
+
+        fun getChild(char: Char): Node? {
+            return children[char]
+        }
+
+        fun isWordEnd(): Boolean {
+            return isWord
+        }
+
+        fun markWordEnd() {
+            isWord = true
+        }
+    }
+
+    protected val root: Node = Node(' ')
+
+    init {
+        words.forEach { insert(it) }
+    }
+
+    fun insert(word: String) {
+        var node = root
+        for (char in word) node = node.addChild(char)
+        node.markWordEnd()
+    }
+
+    fun search(word: String): Boolean {
+        var node = root
+        for (char in word) {
+            if (!node.hasChild(char)) {
+                return false
+            }
+            node = node.getChild(char)!!
+        }
+        return node.isWordEnd()
+    }
+
+}
